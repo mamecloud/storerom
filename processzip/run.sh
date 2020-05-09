0fbad0d45b990636ede70bfdb772b667d58ad436
David Carboni <david@carboni.io>
2020-05-09 21:29:23 +0100

    Rearranging for different test runs.

--- processzip/run.sh
+++ processzip/run.sh
@@ -7,6 +7,8 @@ project_id=$(cat ../project.txt)
 gcloud config set account $account
 gcloud config set project $project_id
 
-gsutil -m rm -r gs://${project_id}-roms/*
+#./deploy.sh
+
+#gsutil -m rm -r gs://${project_id}-roms/*
 #gsutil mb -l europe-west2 -b on gs://${project_id}-roms
 gsutil -m cp -r gs://${project_id}-temp/* gs://${project_id}-roms-upload
