cf082f7a52fc0fff3000222084a74ecdec6eb011
David Carboni <david@carboni.io>
2020-05-09 12:11:29 +0100

    Formatted.

--- sortbysize/function.go
+++ sortbysize/function.go
@@ -1,14 +1,14 @@
 package sortbysize
 
 import (
+	"cloud.google.com/go/pubsub"
 	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
-	"strconv"
-	"cloud.google.com/go/pubsub"
 )
 
 var projectID string = os.Getenv("PROJECT_ID")
@@ -60,7 +60,7 @@ type PubSubMessage struct {
 
 // PublishRom publishes a message about an uploaded rom.
 // The topic the rom is published to depends on the file size.
-// This enables functions with different memory allocationt to handle different file sizes. 
+// This enables functions with different memory allocationt to handle different file sizes.
 func PublishRom(ctx context.Context, e GCSEvent) error {
 
 	object := e.Name
@@ -100,11 +100,11 @@ func size(size string) (megabytes int64, display string) {
 	}
 
 	if bytes < 1024 {
-		display = fmt.Sprintf("%vB", bytes) 
+		display = fmt.Sprintf("%vB", bytes)
 	} else if bytes < (1024 * 1024) {
-		display = fmt.Sprintf("%vK", bytes / 1024) 
+		display = fmt.Sprintf("%vK", bytes/1024)
 	} else {
-		display = fmt.Sprintf("%vM", bytes / (1024 * 1024)) 
+		display = fmt.Sprintf("%vM", bytes/(1024*1024))
 	}
 
 	megabytes = bytes / (1024 * 1024)
@@ -126,7 +126,7 @@ func publish(object string, topicID string) {
 	result := t.Publish(ctx, &pubsub.Message{
 		Data: []byte(object),
 	})
-	
+
 	// The Get method blocks until a server-generated ID or
 	// an error is returned for the published message.
 	id, err := result.Get(ctx)
